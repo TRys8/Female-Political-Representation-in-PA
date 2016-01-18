16224cb61d4f97420dbee430eda97ccb7e5ec338
Waqqas Farooq <wmf23@cornell.edu>
2016-01-17 23:20:21 -0500

    Adding district center coordinates

--- public/heatmap.js
+++ public/heatmap.js
@@ -1,51 +1,11 @@
 
 window.onload = function() {
 
-  $.ajax({
-    type: "GET",
-    url: "2011-Revised-Final-Plan-House.kml",
-    dataType: "xml",
-    success: function(xml) {
-
-      var districtCenters = {};
-
-      $(xml).find('Placemark').each(function() {
-
-        var districtNumber = parseInt($(this).children("name").text().split(/\s/)[1]);
-
-        var bounds = new google.maps.LatLngBounds();
-
-        $(this).find("coordinates").text().split(/\s/).forEach(function(line, index) {
-          if (line.length === 0) {
-            return;
-          }
-
-          var items = line.split(/,/);
-
-          /*if (index === 0) {
-            console.log(districtNumber, items);
-          }*/
-
-          var lng = parseFloat(items[0]);
-          var lat = parseFloat(items[1]);
-
-          bounds.extend(new google.maps.LatLng(lat, lng));
-        });
-
-        districtCenters[districtNumber] = bounds.getCenter();
-      });
-
-      g_districtCenters = districtCenters;
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      $("#status").text("There was a problem in loading district maps: " + textStatus + ", " + errorThrown);
-    },
-    complete: function(jqXHR, textStatus) {
-      var status = $("#status").text();
-      status += " My work here is done: " + textStatus;
-      $("#status").text(status);
-    }
-  });
+  // We are not using KML because I have already calculated
+  // the centers of the disctrics and saved them in the JSON
+  // file.
+  // loadDistrictsFromKML();
+  loadDistrictsFromJSON();
 
 
   var retryCount = 0;
@@ -102,4 +62,78 @@ function initMap() {
     map: map
   });
   districtLayer.parse('2011-Revised-Final-Plan-House.kml');*/
+}
+
+function loadDistrictsFromJSON() {
+  $.ajax({
+    type: "GET",
+    url: "house-districts-centers.json",
+    dataType: "json",
+    success: function(data) {
+
+      var districtCenters = {};
+
+      Object.keys(data).forEach(function(key) {
+        districtCenters[key] = new google.maps.LatLng(data[key].lat, data[key].lng);
+      });
+
+      g_districtCenters = districtCenters;
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $("#status").text("There was a problem in loading district maps: " + textStatus + ", " + errorThrown);
+    },
+    complete: function(jqXHR, textStatus) {
+      var status = $("#status").text();
+      status += " My work here is done: " + textStatus;
+      $("#status").text(status);
+    }
+  });
+}
+
+function loadDistrictsFromKML() {
+  $.ajax({
+    type: "GET",
+    url: "2011-Revised-Final-Plan-House.kml",
+    dataType: "xml",
+    success: function(xml) {
+
+      var districtCenters = {};
+
+      $(xml).find('Placemark').each(function() {
+
+        var districtNumber = parseInt($(this).children("name").text().split(/\s/)[1]);
+
+        var bounds = new google.maps.LatLngBounds();
+
+        $(this).find("coordinates").text().split(/\s/).forEach(function(line, index) {
+          if (line.length === 0) {
+            return;
+          }
+
+          var items = line.split(/,/);
+
+          /*if (index === 0) {
+            console.log(districtNumber, items);
+          }*/
+
+          var lng = parseFloat(items[0]);
+          var lat = parseFloat(items[1]);
+
+          bounds.extend(new google.maps.LatLng(lat, lng));
+        });
+
+        districtCenters[districtNumber] = bounds.getCenter();
+      });
+
+      g_districtCenters = districtCenters;
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $("#status").text("There was a problem in loading district maps: " + textStatus + ", " + errorThrown);
+    },
+    complete: function(jqXHR, textStatus) {
+      var status = $("#status").text();
+      status += " My work here is done: " + textStatus;
+      $("#status").text(status);
+    }
+  });
 }
